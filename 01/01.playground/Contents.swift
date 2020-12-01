67322d2c48349eec91c279f6cc4cb374656f75e3
Martin Grider <martin@abstractpuzzle.com>
2020-12-01 11:40:49 -0600

    after first puzzle

--- 01/01.playground/Contents.swift
+++ 01/01.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
+let contents = try String(contentsOf: url, encoding: String.Encoding.utf8)
+let input: [Int] = contents.split(separator: "\n").map { Int($0) ?? 0 }
+
+//print(input.count)
+
+for i in input {
+	if let i2 = input.first(where: {
+		i + $0 == 2020
+	}) {
+		print(i * i2)
+		break
+	}
+}
+
