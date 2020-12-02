ae4c2bb0a27c67ae6db866677dea0cb24a6fe4a5
Martin Grider <martin@abstractpuzzle.com>
2020-12-02 00:48:07 -0600

    break all the loops.

--- 01/01.playground/Contents.swift
+++ 01/01.playground/Contents.swift
@@ -4,14 +4,37 @@ let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
 let contents = try String(contentsOf: url, encoding: String.Encoding.utf8)
 let input: [Int] = contents.split(separator: "\n").map { Int($0) ?? 0 }
 
-//print(input.count)
-
-for i in input {
-	if let i2 = input.first(where: {
-		i + $0 == 2020
-	}) {
-		print(i * i2)
-		break
+// 01.2
+let start = Date()
+loops: for i in input {
+	for i2 in input {
+		if let i3 = input.first(where: {
+			i + i2 + $0 == 2020
+		}) {
+			print(i * i2 * i3)
+			break loops
+		}
 	}
 }
+let diff = String(format:"%.2f", -start.timeIntervalSinceNow)
+print("Took \(diff) seconds")
+//281473080
+//Took 29.36 seconds
 
+// 01.1
+//for i in input {
+//	if let i2 = input.first(where: {
+//		i + $0 == 2020
+//	}) {
+//		print(i * i2)
+//		break
+//	}
+//}
+
+//func testB(i: Int, i2: Int, i3: Int) -> Bool {
+//	if i + i2 + i3 == 2020 {
+//		print("solution: \( i * i2 * i3 )")
+//		return true
+//	}
+//	return false
+//}
