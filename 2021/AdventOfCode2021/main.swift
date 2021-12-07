9930f3e326584cced47013a90197c5438f968593
Martin Grider <mgrider@gmail.com>
2021-12-06 23:52:32 -0600

    day 7.

--- 2021/AdventOfCode2021/main.swift
+++ 2021/AdventOfCode2021/main.swift
@@ -1,37 +1,37 @@
 import Foundation
 
-print("=== DAY 01 ===")
-let day1 = Day01()
-day1.perform()
-
-print("=== DAY 02 ===")
-let day2 = Day02()
-day2.perform()
-
-print("=== DAY 03 ===")
-let day3 = Day03()
-day3.perform()
-
-print("=== DAY 04 ===")
-let day4 = Day04()
-day4.perform()
-
-print("=== DAY 05 ===")
-let day5 = Day05()
-day5.perform()
-
-print("=== DAY 06 ===")
-let day6 = Day06()
-day6.perform()
-
-//print("=== DAY 07 ===")
-//let day07 = Day07()
-//day07.perform()
+//print("=== DAY 01 ===")
+//let day1 = Day01()
+//day1.perform()
 //
-//print("=== DAY 08 ===")
-//let day08 = Day08()
-//day08.perform()
+//print("=== DAY 02 ===")
+//let day2 = Day02()
+//day2.perform()
 //
+//print("=== DAY 03 ===")
+//let day3 = Day03()
+//day3.perform()
+//
+//print("=== DAY 04 ===")
+//let day4 = Day04()
+//day4.perform()
+//
+//print("=== DAY 05 ===")
+//let day5 = Day05()
+//day5.perform()
+//
+//print("=== DAY 06 ===")
+//let day6 = Day06()
+//day6.perform()
+
+print("=== DAY 07 ===")
+let day07 = Day07()
+day07.perform()
+
+print("=== DAY 08 ===")
+let day08 = Day08()
+day08.perform()
+
 //print("=== DAY 09 ===")
 //let day09 = Day09()
 //day09.perform()
