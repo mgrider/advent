720f8d260a1fc6b5d955e922c2e35703b5e0ef33
Martin Grider <mgrider@gmail.com>
2021-12-08 23:46:25 -0600

    day 9.

--- 2021/AdventOfCode2021/Day09.swift
+++ 2021/AdventOfCode2021/Day09.swift
@@ -2,18 +2,90 @@ import Foundation
 
 class Day09 {
 
+    struct Coord: Equatable {
+        var x: Int
+        var y: Int
+    }
+
     func part1() {
         let input = inputArray()
 
-        let output = input
-        print("part1 = \(output)")
+        var risk = 0
+        for y in 0..<input.count {
+            for x in 0..<input[y].count {
+                guard let height = Int(String(input[y][x])) else {
+                    assertionFailure()
+                    break
+                }
+                // up
+                if y > 0 &&
+                    height >= Int(String(input[y-1][x]))! {
+                    continue
+                }
+                // down
+                if y < input.count - 1 &&
+                    height >= Int(String(input[y+1][x]))! {
+                    continue
+                }
+                // left
+                if x > 0 &&
+                    height >= Int(String(input[y][x-1]))! {
+                    continue
+                }
+                // right
+                if x < input[y].count - 1 &&
+                    height >= Int(String(input[y][x+1]))! {
+                    continue
+                }
+                // otherwise we're in risky business
+                risk = risk + height + 1
+            }
+        }
+
+        print("part1 = \(risk)")
     }
 
     func part2() {
         let input = inputArray()
 
-        let output = input
-        print("part2 = \(output)")
+        var foundBasins = [[Coord]]()
+        for y in 0..<input.count {
+            for x in 0..<input[y].count {
+                if Int(String(input[y][x]))! == 9 {
+                    continue
+                }
+                let currentCoord = Coord(x: x, y: y)
+                if foundBasins.contains(where: { $0.contains(where: { $0 == currentCoord })}) {
+                    continue
+                }
+                let basin = addCoordToBasin(coord: currentCoord, basin: [Coord](), input: input)
+                foundBasins.append(basin)
+            }
+        }
+
+        foundBasins.sort(by: { $0.count > $1.count })
+        let threeLargestBasinSizes = foundBasins[0].count * foundBasins[1].count * foundBasins[2].count
+
+        print("part2 = \(threeLargestBasinSizes)")
+    }
+
+    func addCoordToBasin(coord: Coord, basin: [Coord], input: [String]) -> [Coord] {
+        if basin.contains(coord) {
+            return basin
+        }
+        if coord.x < 0 || coord.y < 0 || coord.y >= input.count || coord.x >= input[coord.y].count {
+            return basin
+        }
+        if Int(String(input[coord.y][coord.x]))! == 9 {
+            return basin
+        }
+        var newBasin = basin
+        newBasin.append(coord)
+        newBasin = addCoordToBasin(coord: Coord(x: coord.x, y: coord.y-1), basin: newBasin, input: input)
+        newBasin = addCoordToBasin(coord: Coord(x: coord.x, y: coord.y+1), basin: newBasin, input: input)
+        newBasin = addCoordToBasin(coord: Coord(x: coord.x+1, y: coord.y), basin: newBasin, input: input)
+        newBasin = addCoordToBasin(coord: Coord(x: coord.x-1, y: coord.y), basin: newBasin, input: input)
+        return newBasin
     }
 
     private func inputArray() -> [String] {
